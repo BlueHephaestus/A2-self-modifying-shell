927530412d4ccd743e7adf54f13ac9e3533cfaf7
blue <bluekroma@gmail.com>
2021-12-06 00:27:06 -0800

    renames

--- network_animate.py
+++ network_animate.py
@@ -12,22 +12,14 @@ class Network():
 
         self.fig, self.ax = plt.subplots()
         self.mat = self.ax.matshow(self.net)
-        #self.mat.autoscale()
-        #plt.colorbar(self.mat, cmap="jet")
-        #self.fig.colorbar(self.mat)
-        #self.cb = self.fig.colorbar(self.mat)
         self.cb = plt.colorbar(self.mat)
-        #self.cb.ax.set_autoscale_on(True)
         self.frames_n = 128
-        # can have an argument for an iterable but i'm fine with having it be live
 
     def update(self, frame_i):
-        print("update", frame_i)
         # each timestep
         i,j = randint(0,15), randint(0,15)
         self.net[i,j] = randint(0,10)
         self.mat.set_data(self.net)
-        #FUCK
         #update colorbar limits each time
         self.mat.set_clim(np.amin(self.net), np.amax(self.net))
 
