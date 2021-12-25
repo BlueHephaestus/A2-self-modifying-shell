b333981446fb8f07161de1e928a861aff4190bdf
blue <bluekroma@gmail.com>
2021-12-24 19:14:09 -0800

    Quick commit of working animation for rng RNN, with edges changing each frame. Good demo of how this will work with UI. I'd like to be coding this faster, but can't since my fucking wrist is acting up - i got surgery to fix this, not have it worse, aaaaa. *sigh*. All i want for christmas is to not be injured anymore.

--- network_animate.py
+++ network_animate.py
@@ -2,6 +2,8 @@ import matplotlib.animation
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import default_rng
+rng = default_rng()
 
 
 # USEFUL BITS OF CODE
@@ -30,22 +32,29 @@ As input, takes data about how large the network will be.
 
 """
 class NetworkAnimator():
-    def __init__(self, net, s):
-        # pos = nx.spring_layout(G)
+    def __init__(self, net, n):
+        """
+        Animation engine for network structure.
+            Designed to be non-blocking and event-driven, such that it will update the rendered view of the
+            network whenever it is called by the network backend to render.
+
+        :param net: network structure to render
+        :param n: size of hex structure grid, e.g. n=16 would be a 16x16 grid.
+        """
         self.n = n
-        self.nodes = [f"{i//n},{i%n}" for i in range(n)]
-        self.node_idxs = [(i//n, i%n) for i in range(n)]
-        self.G = nx.DiGraph()
-        self.G.add_nodes_from(self.nodes)
-        #val_map = {'A':1.0}
+        #self.nodes = [f"{i//n},{i%n}" for i in range(n)]
+        #self.node_idxs = [(i//n, i%n) for i in range(n)]
+
+        #self.G = nx.DiGraph()
+        self.net = net
+        #self.G.add_nodes_from(self.nodes)
 
         self.fig, self.ax = plt.subplots(figsize=(n // 2, n // 2))
-
         #self.ax.clear()
         #pos = {'A': (1, num), 'B': (3, 3), 'C': (1, 2), 'D': (5, 2), 'E': (6, 1), 'F': (9, 0), 'G': (3, 1), 'H': (4, 4)}
-        pos = {node:node_i for node,node_i in zip(self.nodes, self.node_idxs)}
-        #self.nptr = nx.draw_networkx_nodes(self.G, pos, cmap=plt.get_cmap('jet'), node_size=500, node_shape='s', ax=self.ax)
-        nx.draw_networkx_labels(self.G, pos, font_size=8)
+        #pos = {node:node_i for node,node_i in zip(self.nodes, self.node_idxs)}
+        #nx.draw_networkx_nodes(self.G, pos, cmap=plt.get_cmap('jet'), node_size=500, node_shape='s', ax=self.ax)
+        #nx.draw_networkx_labels(self.G, pos, font_size=8)
 
         # Plot metadata / config
         #self.ax.set_title("Frame %d:    "%(num+1), fontweight="bold")
@@ -61,21 +70,11 @@ class NetworkAnimator():
         self.ax.grid(which='minor')
         self.num = 0
 
-    def update(self, num):
-        print("update")
-        self.node_idxs2 = [(i//n+num%n, i%n) for i in range(n)]
-        pos = {node:node_i for node,node_i in zip(self.nodes, self.node_idxs2)}
-        if num == 10:
-            self.G.add_node("A")
-        if num >= 10 and num < 20:
-            pos["A"] = (14, 14)
-
-        if num == 20:
-            self.G.remove_node("A")
-
-
-
-        self.nptr = nx.draw_networkx_nodes(self.G, pos, cmap=plt.get_cmap('jet'), node_size=500, node_shape='s', ax=self.ax)
+    def update(self, pos):
+        self.ax.clear()
+        nx.draw_networkx_nodes(self.net, pos, cmap=plt.get_cmap('jet'), node_size=150, node_shape='s', ax=self.ax)
+        nx.draw_networkx_labels(self.net, pos, font_size=6, ax=self.ax)
+        nx.draw_networkx_edges(self.net, pos, ax=self.ax)
 
         # Graph bookkeeping - keep it from breaking
         self.ax.set_xlim((-.5,n-.5))
@@ -84,40 +83,84 @@ class NetworkAnimator():
         self.ax.set_yticks(np.arange(n))
         self.ax.tick_params(left=True, top=True, labelleft=True, labeltop=True)
         self.ax.xaxis.tick_top()
+        self.ax.xaxis.set_minor_locator(matplotlib.ticker.FixedLocator(np.arange(n-1)+0.5))
+        self.ax.yaxis.set_minor_locator(matplotlib.ticker.FixedLocator(np.arange(n-1)+0.5))
+        self.ax.grid(which='minor')
 
-        # return artist objects for matplotlibs animations
-        return self.nptr.findobj()
 
-    def main(self, draw=False):
-        # Keeping blit as False, was working to make blit True work but it just is coming up to too much work for
-        # what is ultimately a convenience feature for visualization. Don't need this to run that fast, maybe.
-        self.ani = matplotlib.animation.FuncAnimation(self.fig, self.update, frames=60, interval=50, repeat=True, blit=True)
+def rng_int(n):
+    """
+    Generate random int from 0->n, inclusive
+    """
+    return rng.integers(n+1)
 
-# TODO
-# increase modularities and abilities of the network
-# objectify
+def rng_2d_coord_choice(w,h,n):
+    """
+    :param w: width of 2d matrix
+    :param h: height of 2d matrix
+    :param n: number of coords to select
+    :return: Uniformly distributed coordinates across 2d matrix, without repetition/replacement
+        Shape will be (n,2) s.t. each entry is a 2d coordinate pair, row-col
+    """
+    coords_1d = rng.choice(w*h, size=n, replace=False)
+    coords_2d = np.column_stack((coords_1d//w,coords_1d%w))
+    return coords_2d
 
-n = 32
-na = NetworkAnimator(n,n)
-na.main()
+def rng_rnn(n):
+    """
+    :param n: Size of square bounds, 0-n
+    :return: Randomly generated RNN within bounds.
+    """
+    rnn = nx.DiGraph()
+    # Generate number of nodes and edges
+    # Original distributions; but since the set of all possible RNNs mostly have a HUGE amount of connections,
+    # and we only want to view some small nets in the sandbox, I sqrt'd the usual dists.
+    # nodes_n = rng_int(n**2)
+    # edges_n = rng_int(nodes_n**2)
+    #nodes_n = rng_int(n)
+    nodes_n = 16
+    edges_n = rng_int(nodes_n)
+    print(f"Nodes: {nodes_n}")
+    print(f"Edges: {edges_n}")
+    print()
+
+    ### NODES ###
+
+    # to get the node coordinates - we use their coordinates as their label, btw
+    # Get node coordinates in square bounds
+    node_idxs = rng_2d_coord_choice(n,n,nodes_n)
+    node_labels = [f"{i},{j}" for i,j in node_idxs]
+
+    # pos takes x,y not row,col so we invert with [::-1]
+    node_pos = {node_label:node_idx[::-1] for node_label,node_idx in zip(node_labels,node_idxs)}
+    rnn.add_nodes_from(node_labels)
+
+    ### EDGES ###
+    # Get edge indices in node *ADJACENCY MATRIX*, not the square bounds this rnn exists in.
+    # s.t. indices correspond to nodes in the nodes array.
+    edge_idxs = rng_2d_coord_choice(nodes_n,nodes_n,edges_n)
+
+    # Get node labels from adjacency matrix indices to form full edge labels
+    # reminder that an edge label is the form ("<src node label>", "<dst node label>")
+    edge_labels = [(node_labels[src_i], node_labels[dst_i]) for src_i, dst_i in edge_idxs]
+    rnn.add_edges_from(edge_labels)
+
+    return rnn, node_pos, node_labels, nodes_n
+    #nx.draw_networkx_nodes(rnn, pos, cmap=plt.get_cmap('jet'), node_size=500, node_shape='s')
+
+n = 16
+net, pos, node_labels, nodes_n = rng_rnn(n)
+na = NetworkAnimator(net, n)
+for _ in range(100):
+    na.net.remove_edges_from([e for e in na.net.edges()])
+    edge_idxs = rng_2d_coord_choice(nodes_n,nodes_n,rng_int(nodes_n))
+    edge_labels = [(node_labels[src_i], node_labels[dst_i]) for src_i, dst_i in edge_idxs]
+    na.net.add_edges_from(edge_labels)
+
+    plt.show(block=False)
+    na.update(pos)
+    plt.pause(1.001)
+    #plt.show()
+
+
 plt.show()
-#
-# # TODO
-# # increase modularities and abilities of the network
-# # objectify
-#
-# #plt.ion()
-# #plt.show()
-# n = 32
-# na = NetworkAnimator(n,n)
-# plt.show(block=False)
-# i = 0
-# import time
-# while True:
-#     if i % 2 == 0:
-#         #na.ax.clear()
-#         #plt.clf()
-#         na.update(i)
-#     i+=1
-#     plt.pause(.1)
-#     #time.sleep(1)
